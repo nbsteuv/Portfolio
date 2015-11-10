60e7945a72736a275284971a6a8c71935bee5d99
Nick Steuver <nbsteuv@gmail.com>
2015-11-10 12:50:09 -0500

    Added descriptions and technology used content.

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,7 @@ angular.module('portfolio', ['ngRoute', 'ngAnimate', 'appRoutes', 'PortfolioCTRL
 	
 	$rootScope.gridDisplay = true;
 
-	$rootScope.current = 1;
+	$rootScope.current = 0;
 
 	$rootScope.projects = [
 		{
@@ -12,9 +12,9 @@ angular.module('portfolio', ['ngRoute', 'ngAnimate', 'appRoutes', 'PortfolioCTRL
 			name: "Avoid The Circles",
 			image: "img/AvoidTheCirclesSnapshot.JPG",
 			link: "link",
-			tech: "Tech used",
+			tech: ["HTML", "CSS", "Javascript", "jQuery"],
 			tagline: "A simple browser game using jQuery.",
-			description: "Description here.",
+			description: "Keep your mouse pointer away from the growing number of circles generated through Javascript, but don't leave the gaming field or it's game over. Complete with a timer and high score list.",
 			github: "https://github.com/nbsteuv/AvoidTheCircles"
 		},
 		
@@ -23,9 +23,9 @@ angular.module('portfolio', ['ngRoute', 'ngAnimate', 'appRoutes', 'PortfolioCTRL
 			name: "Workout App in C# and .NET",
 			image: "img/CSharpWorkoutAppSnapshot.JPG",
 			link: "link",
-			tech: "Tech used",
+			tech: ["C#", ".NET Framework", "WPF", "Visual Studio"],
 			tagline: "An application for storing weightlifting data.",
-			description: "Description here.",
+			description: "This .NET application allows users to input workout data, save it in CSV file format, and retrieve it during future sessions for display within the app.",
 			github: "https://github.com/nbsteuv/WorkoutApplicationCSharpWPF"
 		},
 
@@ -34,9 +34,9 @@ angular.module('portfolio', ['ngRoute', 'ngAnimate', 'appRoutes', 'PortfolioCTRL
 			name: "MEAN Stack Workout App",
 			image: "img/AngularWorkoutAppSnapshot.JPG",
 			link: "link",
-			tech: "Tech used",
+			tech: ["AngularJS", "MongoDB through MongoLab", "Express", "NodeJS", "SHA1 hash encryption through CryptoJS", "AES data encryption through CryptoJS", "User authentication through Passport and Cookie Parser", "Twitter Bootstrap"],
 			tagline: "A MEAN Stack application for storing weightlifting data.",
-			description: "Description here.",
+			description: "An online iteration of the C# .NET workout app, this application allows users to create an account with a SHA1-encrypted password and save their AES-encrypted workout data to a MongoLab-hosted Mongo database.",
 			github: "https://github.com/nbsteuv/WorkoutAppAngularJS"
 		}
 	];
