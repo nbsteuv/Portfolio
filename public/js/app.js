017a0ad88f6134cc2973a43319f1c684db8d5c1e
nbsteuv <nbsteuv@gmail.com>
2017-02-02 22:34:10 -0500

    Added tile game data

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 angular.module('portfolio', ['ngRoute', 'ngAnimate', 'appRoutes', 'PortfolioCTRL', 'ProjectDisplayCTRL'])
 
 .run(function($rootScope){
-	
+
 	$rootScope.gridDisplay = true;
 
 	$rootScope.current = 0;
@@ -17,12 +17,12 @@ angular.module('portfolio', ['ngRoute', 'ngAnimate', 'appRoutes', 'PortfolioCTRL
 			description: "Keep your mouse pointer away from the growing number of circles generated through Javascript, but don't leave the gaming field or it's game over. Complete with a timer and high score list.",
 			github: "https://github.com/nbsteuv/AvoidTheCircles"
 		},
-		
+
 		{
 			id: 1,
 			name: "Workout App in C# and .NET",
 			image: "img/CSharpWorkoutAppSnapshot.JPG",
-			link: "/WorkoutAppCSharp/",
+			link: "/WorkoutAppCSharp",
 			tech: ["C#", ".NET Framework", "WPF", "Visual Studio"],
 			tagline: "An application for storing weightlifting data.",
 			description: "This .NET application allows users to input workout data, save it in CSV file format, and retrieve it during future sessions for display within the app. To view, click the link below, unzip the folder, and run the EXE file.",
@@ -38,6 +38,17 @@ angular.module('portfolio', ['ngRoute', 'ngAnimate', 'appRoutes', 'PortfolioCTRL
 			tagline: "A MEAN Stack application for storing weightlifting data.",
 			description: "An online iteration of the C# .NET workout app, this application allows users to create an account with a SHA1-encrypted password and save their workout data to a MongoLab-hosted Mongo database.",
 			github: "https://github.com/nbsteuv/WorkoutAppAngularJS"
-		}
+		},
+
+		{
+			id: 3,
+			name: "Sliding Tiles Word Game",
+			image: "img/TileGameDemo.JPG",
+			link: "/TileGameDemo",
+			tech: ["C#", ".Net Framework", "Unity Game Engine v5.5"],
+			tagline: "Form the randomly-generated words with the sliding tiles to win the game.",
+			description: "A sliding tile game created in C# .NET with the Unity game engine with a Web GL implementation.",
+			github: "https://github.com/nbsteuv/Tile-Game"
+		},
 	];
 });
